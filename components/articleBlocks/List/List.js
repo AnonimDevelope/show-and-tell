f4e6a20e22418b98edfa12f3bd0a70006f91abd9
Razvik <mrrazorhd@gmail.com>
2021-07-04 18:15:56 +0300

    Style fix

--- components/articleBlocks/List/List.js
+++ components/articleBlocks/List/List.js
@@ -1,26 +1,27 @@
-import React from "react"
-import { Typography } from "antd"
+import React from "react";
+import { Typography } from "antd";
+import { stripHtml } from "string-strip-html";
 
 const List = ({ items, style }) => {
-  const { Paragraph } = Typography
+  const { Paragraph } = Typography;
 
   return (
     <Paragraph style={{ fontSize: 20 }}>
       {style === "ordered" ? (
         <ol>
           {items.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>{stripHtml(item).result}</li>
           ))}
         </ol>
       ) : (
         <ul>
           {items.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>{stripHtml(item).result}</li>
           ))}
         </ul>
       )}
     </Paragraph>
-  )
-}
+  );
+};
 
-export default List
+export default List;
